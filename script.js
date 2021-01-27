2308b1b84ddcf45c96fbf2b8945646447a2abab7
kierajcullen <kierajcullen@gmail.com>
2021-01-26 21:33:36 -0500

    ajax call

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 var appKey = "f6ebefb8e953ba8b5064c032bfe9e61f";
 var appID = "e24e99ff";
 //add search parameter
-var queryURL = `https://api.edamam.com/api/nutrition-details?app_id=${appID}&app_key=${appKey}`;
+var queryURL = `https://api.edamam.com/search?q=chicken&app_id=${appID}&app_key=${appKey}`;
 $.ajax({
   url: queryURL,
   method: "GET",
 }).then(function (response) {
   console.log(queryURL);
-  console.log(response);
+  // console.log(response);
+  console.log(response.hits);
 });
